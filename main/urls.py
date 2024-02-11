9f381bec429af306facb0c0fe8085fc67b59931b
Omoshlawi <lawiomosh3@gmail.com>
2024-02-11 13:57:16 +0300

    Modeled user profile and configured drf pagination

--- main/urls.py
+++ main/urls.py
@@ -17,8 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-        path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls'))
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Apple Master Admin"
+admin.site.site_title = "Apple Master Admin"
