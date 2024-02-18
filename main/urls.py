ba197aa3501fd986c85ed2cfe1101113c2b3deee
Omoshlawi <lawiomosh3@gmail.com>
2024-02-18 16:31:35 +0300

    Added model serializers, viewsets and urls

--- main/urls.py
+++ main/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('users/', include('users.urls')),
+    path('hotels/', include('hotels.urls', namespace="hotels")),
 ]
 
 
