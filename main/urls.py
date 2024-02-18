5839c5458df891a678c0238df04fdab16eb9d312
Omoshlawi <lawiomosh3@gmail.com>
2024-02-18 13:36:45 +0300

    Added hotel app,modeled and registered the models in admin panel

--- main/urls.py
+++ main/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('users/', include('users.urls')),
 ]
 
 
