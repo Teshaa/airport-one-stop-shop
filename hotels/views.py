e4df6f0aaa79d956d814b9b0f9992016dbc4cdc9
Teshaa <tk.wanja@gmail.com>
2024-03-26 19:09:10 +0300

    Using tags and workig fine

--- hotels/views.py
+++ hotels/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from taggit.models import Tag
+
 
 from hotels.models import Hotel, Room, RoomType, Reservation
-from hotels.serializers import HotelSerializer, RoomSerializer, RoomTypeSerializer, ReservationSerializer
+from hotels.serializers import HotelSerializer, RoomSerializer, RoomTypeSerializer, ReservationSerializer, TagSerializer
 
 
 # Create your views here.
@@ -26,3 +28,8 @@ class RoomTypeViewSet(viewsets.ModelViewSet):
 class ReservationViewSet(viewsets.ModelViewSet):
     serializer_class = ReservationSerializer
     queryset = Reservation.objects.all()
+
+
+class TagsViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
