4abdb46f51835559623679d0959ce743903ac865
Teshaa <tk.wanja@gmail.com>
2024-03-27 20:00:58 +0300

    Added services model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Terminal(models.Model):
+    name = models.CharField(max_length=191)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Service(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="service")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
