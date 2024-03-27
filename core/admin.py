7549d687f3e0cdd5d35ff5150a1444e483c6b104
Omoshlawi <lawiomosh3@gmail.com>
2024-03-28 01:00:08 +0300

    Merge latest

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from core.models import Service, Terminal
+
 # Register your models here.
+
+@admin.register(Terminal)
+class TerminalAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "created_at", "updated_at")
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "created_at", "updated_at")
--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
+from core.models import Service, Terminal
+from core.serializers import ServiceSerializer, TerminalSerializer
 # Create your views here.
+
+
+class TerminalViewSet(viewsets.ModelViewSet):
+    serializer_class = TerminalSerializer
+    queryset = Terminal.objects.all()
+
+class ServiceViewSet(viewsets.ModelViewSet):
+    serializer_class = ServiceSerializer
+    queryset = Service.objects.all()
